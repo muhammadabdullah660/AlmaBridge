2662d6c43dce9210fb6aa52e3a6c8582012452ca
Muhammad-ShahzaibIjaz <muhammadshahzaibijaz34@gmail.com>
2024-12-22 01:21:39 +0500

    Update Features AL-10

--- backend/python_services/app.py
+++ backend/python_services/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from routes.profileRoutes import profile_routes
+from routes.resumeRoutes import resume_routes
 
 app = Flask(__name__)
 CORS(app)
 
 
 app.register_blueprint(profile_routes, url_prefix="/api")
+app.register_blueprint(resume_routes, url_prefix="/api")
 
 if __name__ == "__main__":
     app.run(debug=True)
