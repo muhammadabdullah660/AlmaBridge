0d39f3c8f7d550ae01103f58b93f1a405c71c6d6
Fatima Awais <fatima.awais2003@gmail.com>
2025-06-02 00:14:47 +0500

    AI matchmaking completed AL-29

--- almabridge_web/src/components/dashboard/suggestions/Suggestions.tsx
+++ almabridge_web/src/components/dashboard/suggestions/Suggestions.tsx
@@ -73,6 +73,12 @@ function TeamMemberCard({ member }: { member: AlumniSuggestions }) {
         <p className="text-xs text-gray-500 text-center line-clamp-6">
           {member.about}
         </p>
+
+        {/* Connect Button */}
+      <button className="bg-[#0077B5] text-white font-semibold px-4 py-2 rounded-full text-sm hover:bg-[#005f8b] transition duration-300">
+        Connect
+      </button>
+
       </div>
     </motion.div>
   );
